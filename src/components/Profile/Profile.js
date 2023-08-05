c44c51cede782153ed6214ebdcfb66dd62577c72
SvAmi1 <svmoshko877@gmail.com>
2023-08-05 14:07:27 +0300

    sm

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,31 @@
-import user from ../data.user.json;
-import {Wrapper} from  Profiler.styled.js;
+// import user from "../data/user.json";
 
-export const Description = ({ item: {username, avatar, tag, location}}) => {
-  return (
-    <Wrapper>
-      <img src={avatar} alt={username}photo/>
-      <p>{username}</p>
-      <p>@{tag}</p>
-      <p>{location}</p>
-    </Wrapper>
+   import { Description, Avatar, Name, Tag, Location, Profile } from "./Profile.styled";
+
+export const ProfileSM = ({ user: {username, avatar, tag, location, stats: {followers, views, likes}}}) => {
+  return ( 
+  <Profile>
+    <Description>
+      <Avatar src={avatar} alt={username}photo/>
+      <Name>{username}</Name>
+      <Tag>@{tag}</Tag>
+      <Location>{location}</Location>
+    </Description>
+
+    <ul>
+      <li>
+        <span>Followers</span>
+        <span>{followers}</span>
+      </li>
+      <li>
+        <span>Views</span>
+        <span>{views}</span>
+      </li>
+      <li>
+        <span>Likes</span>
+        <span>{likes}</span>
+      </li>
+    </ul>
+  </Profile>
   )
 }
--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -1,5 +1,30 @@
-export const Wrapper = styled.div`
+export const Profile = styled.div`
+display: flex;
+alignItems: 'center';
+/* backgroundColor: blue; */
+`;
+
+export const Description = styled.div`
 display: flex;
 alignItems: 'center';
 backgroundColor: blue;
+`;
+
+export const Avatar = styled.img`
+width: 200px;
+`;
+
+export const Name = styled.p`
+font-size: 20px;
+color: black;
+`;
+
+export const Tag = styled.p`
+font-size: 16px;
+color: gray;
+`;
+
+export const Location = styled.p`
+font-size: 18px;
+color: gray;
 `;
